7603fdb0d40720a1d9b1cd6a36b8220fa57386ef
CyberCadence <diljithv7@gmail.com>
2022-09-20 11:57:59 +0530

    fl_chart _listed

--- coinBase_clone/lib/pages/coin_pages.dart
+++ coinBase_clone/lib/pages/coin_pages.dart
@@ -1,116 +1,179 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:disney_clone/utils/theme.dart';
-import 'package:flutter/material.dart';
+
 
 import 'package:disney_clone/models/coinModel.dart';
+import 'package:disney_clone/models/coin_detailsmodel.dart';
+import 'package:disney_clone/utils/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
 
+ 
+import '../services/coin_repository.dart';
+ 
 class CoinPage extends StatefulWidget {
-  CoinPage({
-    Key? key,
-    required this.coin,
-  }) : super(key: key);
   final Coin coin;
+  const CoinPage({required this.coin, Key? key}) : super(key: key);
 
   @override
   State<CoinPage> createState() => _CoinPageState();
 }
 
 class _CoinPageState extends State<CoinPage> {
+  late Future<List<CoinData>> _getCoinHourly;
+
   late double _coinPrice;
+
+  convertToSpots(List<CoinData> coinData) {
+    final List<FlSpot> spots = [];
+    double maxPrice = 0.0;
+    double minPrice = double.maxFinite;
+    double maxTime = 0.0;
+    double minTime = double.maxFinite;
+    const normalizer = 10000;
+
+    // for loop over coin data
+    for (var i = 0; i < coinData.length; i++) {
+      final CoinData coin = coinData[i];
+      final double price = coin.high.toDouble();
+      final double time = coin.time.toDouble() / normalizer;
+      if (price > maxPrice) {
+        maxPrice = price;
+      }
+      if (price < minPrice) {
+        minPrice = price;
+      }
+      if (time > maxTime) {
+        maxTime = time;
+      }
+      if (time < minTime) {
+        minTime = time;
+      }
+      spots.add(FlSpot(time, price));
+    }
+    return [spots, maxPrice, maxTime, minPrice, minTime];
+  }
+
   @override
   void initState() {
     super.initState();
+    _getCoinHourly = CoinRepository.getCoinHourlyData(widget.coin.ticker);
     _coinPrice = widget.coin.price;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text(widget.coin.fullName),
+        backgroundColor: Colors.white,
         elevation: 0,
+        title: Text(
+          widget.coin.fullName,
+          style: const TextStyle(color: Colors.black),
+        ),
         leading: IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: (() => Navigator.pop(context))),
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
+          onPressed: () => Navigator.pop(context),
+        ),
       ),
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: ListView(children: [
-          Text(
-            '${widget.coin.fullName}price',
-            style: TextStyle(color: Colors.grey.shade700, fontSize: 15),
-          ),
-          Text(
-            "\$${_coinPrice.toStringAsFixed(2)}",
-            style: const TextStyle(
-                color: Colors.black, fontSize: 28, fontWeight: FontWeight.bold),
-          ),
-          const SizedBox(
-            height: 50,
-          ),
-          Text(
-            widget.coin.fullName,
-            style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-          ),
-          Text(
-            widget.coin.ticker,
-            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: 1,
-            color: Colors.grey.shade300,
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Container(
-            width: MediaQuery.of(context).size.width,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Additional information :',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  "Algorithm : ${widget.coin.algorithm}",
-                  style: const TextStyle(
-                    fontSize: 15,
-                  ),
-                ),Text(
-                  "Block number : ${widget.coin.blockNumber}",
-                  style: const TextStyle(
-                    fontSize: 15,
-                  ),
-                ),Text(
-                  "Block reward : ${widget.coin.blockReward}",
-                  style: const TextStyle(
-                    fontSize: 15,
-                  ),
-                ),Text(
-                  "Block time : ${widget.coin.blockTime}",
-                  style: const TextStyle(
-                    fontSize: 15,
-                  ),
-                ),Text(
-                  "Max supply : ${widget.coin.maxSupply}",
-                  style: const TextStyle(
-                    fontSize: 15,
-                  ),
-                )
-              ],
-            ),
-          )
-        ,const SizedBox(height: 20,)
-             , ElevatedButton(
+        child: Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: ListView(
+            children: [
+              Text(
+                "${widget.coin.fullName} price",
+                style: TextStyle(color: Colors.grey.shade700, fontSize: 14),
+              ),
+              Text(
+                "\$${_coinPrice.toStringAsFixed(2)}",
+                style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              FutureBuilder<List<CoinData>>(
+                future: _getCoinHourly,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done &&
+                      snapshot.data != null) {
+                    final f = convertToSpots(snapshot.data!);
+                    return CoinHourlyChart(
+                      flspots: f[0],
+                      maxPrice: f[1],
+                      maxTime: f[2],
+                      minPrice: f[3],
+                      minTime: f[4],
+                      priceCallback: (d) {
+                        setState(() {
+                          _coinPrice = d ?? widget.coin.price;
+                        });
+                      },
+                    );
+                  }
+                  return const Text("Loading");
+                },
+              ),
+              Text(
+                widget.coin.fullName,
+                style:
+                    const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                widget.coin.ticker,
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width,
+                height: 1,
+                color: Colors.grey.shade300,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Additional information:",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Text(
+                "Algorithm: ${widget.coin.algorithm}",
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+              ),
+              Text(
+                "Block number: ${widget.coin.blockNumber.toStringAsFixed(2)}",
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+              ),
+              Text(
+                "Block reward: ${widget.coin.blockReward.toStringAsFixed(2)}",
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+              ),
+              Text(
+                "Block time: ${widget.coin.blockTime.toStringAsFixed(2)}",
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+              ),
+              Text(
+                "Max supply: ${widget.coin.maxSupply}",
+                style:
+                    const TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+              ),
+              const SizedBox(
+                height: 25,
+              ),
+              // create a blue button that centers the text and has the entire width of the screen
+              ElevatedButton(
                 style: ButtonStyle(
                     shape: MaterialStateProperty.all(RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(25.0))),
@@ -118,15 +181,103 @@ class _CoinPageState extends State<CoinPage> {
                         const EdgeInsets.symmetric(
                             horizontal: 0, vertical: 15)),
                     backgroundColor:
-                        MaterialStateProperty.all( CoinbaseTheme.color)),
+                        MaterialStateProperty.all(CoinbaseTheme.color)),
                 child: const Text(
                   "Trade",
                   style: TextStyle(color: Colors.white, fontSize: 18),
                 ),
                 onPressed: () {},
               ),
-]),
-      )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CoinHourlyChart extends StatefulWidget {
+  final List<FlSpot> flspots;
+  final double maxPrice;
+  final double maxTime;
+  final double minPrice;
+  final double minTime;
+  final Function(double?) priceCallback;
+
+  const CoinHourlyChart(
+      {required this.flspots,
+      required this.maxPrice,
+      required this.maxTime,
+      required this.minPrice,
+      required this.minTime,
+      required this.priceCallback,
+      Key? key})
+      : super(key: key);
+
+  @override
+  _CoinHourlyChartState createState() => _CoinHourlyChartState();
+}
+
+class _CoinHourlyChartState extends State<CoinHourlyChart> {
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1.70,
+      child: LineChart(
+        LineChartData(
+          backgroundColor: Colors.transparent,
+          gridData: FlGridData(
+            show: false,
+            drawVerticalLine: true,
+            horizontalInterval: 1,
+            verticalInterval: 1,
+          ),
+          lineTouchData: LineTouchData(
+            enabled: true,
+            touchCallback: (c, cc) {
+              widget.priceCallback(cc?.lineBarSpots?[0].y);
+            },
+            touchTooltipData: LineTouchTooltipData(
+              tooltipBgColor: Colors.transparent,
+              tooltipPadding: const EdgeInsets.all(10),
+            ),
+          ),
+          showingTooltipIndicators: [],
+          titlesData: FlTitlesData(
+            show: true,
+            rightTitles: AxisTitles(
+              sideTitles: SideTitles(showTitles: false),
+            ),
+            topTitles: AxisTitles(
+              sideTitles: SideTitles(showTitles: false),
+            ),
+            bottomTitles: AxisTitles(
+              sideTitles: SideTitles(showTitles: false),
+            ),
+            leftTitles: AxisTitles(
+              sideTitles: SideTitles(showTitles: false),
+            ),
+          ),
+          borderData: FlBorderData(
+              show: true, border: Border.all(color: Colors.white, width: 0)),
+          minX: widget.minTime,
+          maxX: widget.maxTime,
+          minY: widget.minPrice,
+          maxY: widget.maxPrice,
+          lineBarsData: [
+            LineChartBarData(
+              spots: widget.flspots,
+              isCurved: false,
+              color: CoinbaseTheme.color,
+              barWidth: 2,
+              isStrokeCapRound: false,
+              dotData: FlDotData(
+                show: false,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- coinBase_clone/lib/services/coin_repository.dart
+++ coinBase_clone/lib/services/coin_repository.dart
@@ -2,88 +2,56 @@ import 'dart:convert';
 
 import 'package:disney_clone/models/coin_detailsmodel.dart';
 import 'package:disney_clone/models/data_error.dart';
-import 'package:http/http.dart'as http;
+import 'package:http/http.dart' as http;
 
 import '../models/coinModel.dart';
 
-class CoinRepository{
+class CoinRepository {
+  static const String _baseUrl = 'https://min-api.cryptocompare.com/';
 
-static const String _baseUrl='https://min-api.cryptocompare.com/';
-
-
-Future<List<Coin>>getCoin()async{
-
-const requestUrl =
+  Future<List<Coin>> getCoin() async {
+    const requestUrl =
         '${_baseUrl}data/top/totalvolfull?limit=25&tsym=USD&page=0';
 
+    try {
+      final response = await http.get(Uri.parse(requestUrl));
 
+      if (response.statusCode == 200) {
+        final decodeddata = jsonDecode(response.body) as Map<String, dynamic>;
+        final data = decodeddata['Data'] as List<dynamic>;
 
-try {
-  final response=await http.get(Uri.parse(requestUrl));
+        return data.map((e) {
+          return Coin.fromMap(e);
+        }).toList();
+      } else {
+        throw Exception('failed to get data');
+      }
+    } catch (e) {
+      throw DataError(message: e.toString());
+    }
+  }
 
+ 
 
+static Future<List<CoinData>> getCoinHourlyData(String ticker) async {
+    final requestUrl =
+        '${_baseUrl}data/v2/histohour?fsym=$ticker&tsym=USD&limit=25';
 
-if(response.statusCode==200){
+    try {
+      final response = await http.Client().get(Uri.parse(requestUrl));
+      if (response.statusCode == 200) {
+        final json = jsonDecode(response.body) as Map<String, dynamic>;
+        final data = json['Data']['Data'] as List<dynamic>;
 
-final decodeddata=  jsonDecode(response.body)as Map<String,dynamic>;
-  final data= decodeddata['Data']as List<dynamic>;
-  
-
-  return data.map((e) {
-return Coin.fromMap(e);
-
-  }).toList();
-
-print(data);
-
-  
-
-
-}else{
-  throw Exception('failed to get data');
-}
-
-
-} catch (e) {
-  
-
-throw DataError(message: e.toString());
-
-}
-
-}
-
-
-Future<List<CoinData>>getHourlydata(String ticker)async{
-
-try {
-
-
-    http.Response response= await http.get(Uri.parse(_baseUrl+'/data/v2/histohour?fsym=$ticker&tsym=USD&limit=25'));
-
-
-if(response.statusCode==200){
-
- final json=   jsonDecode(response.body)as Map<String,dynamic> ;
-  final data=  json['Data']['Data']as List<dynamic>;
-
-  return data.map((e) => CoinData.fromMap(e)).toList();
-
-
-}
-else{
-
-
-  throw Exception('Failed to load details');
-}
-  
-} catch (e) {
-
-
- throw DataError(message: e.toString());
-
-}
-
-}
+        return data.map((e) {
+          return CoinData.fromMap(e);
+        }).toList();
+      } else {
+        throw Exception('Failed to load currencies');
+      }
+    } catch (err) {
+      throw DataError(message: err.toString());
+    }
+  }
 
 }
